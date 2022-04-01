d7f6993cd0a9a62607a96db31db21f97ea3252fa
Eden <Edentad3222@gmail.com>
2022-04-01 18:18:21 +0200

    Simplecalculater

---
+++ src/main/java/pl/vistula/Simplecalculater.java
@@ -0,0 +1,23 @@
+package pl.vistula;
+
+public class Simplecalculater implements Calculator {
+    @Override
+    public double add(double first, double second) {
+        return 0;
+    }
+
+    @Override
+    public double sub(double first, double second) {
+        return 0;
+    }
+
+    @Override
+    public double mul(double first, double second) {
+        return 0;
+    }
+
+    @Override
+    public double div(double first, double second) {
+        return 0;
+    }
+}
