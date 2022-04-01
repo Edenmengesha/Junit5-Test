9e3495f53cced8cef39163da164f0a320beb61f9
Eden <edentad3222@gmail.com>
2022-04-01 18:30:21 +0200

    Git commit

--- src/main/java/pl/vistula/Simplecalculater.java
+++ src/main/java/pl/vistula/Simplecalculater.java
@@ -3,21 +3,21 @@ package pl.vistula;
 public class Simplecalculater implements Calculator {
     @Override
     public double add(double first, double second) {
-        return first + second;
+        return 0;
     }
 
     @Override
     public double sub(double first, double second) {
-        return first - second;
+        return 0;
     }
 
     @Override
     public double mul(double first, double second) {
-        return first * second;
+        return 0;
     }
 
     @Override
     public double div(double first, double second) {
-        return first / second ;
+        return 0;
     }
 }
