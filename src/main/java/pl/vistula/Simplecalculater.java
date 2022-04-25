d4348d373fe703edaa57fe596d11a896bd33cc7a
EdenMengesha52844 <100151122+Edenmengesha@users.noreply.github.com>
2022-04-25 20:13:53 +0200

    Revert "Git commit"
    This reverts commit 9e3495f53cced8cef39163da164f0a320beb61f9.

--- src/main/java/pl/vistula/Simplecalculater.java
+++ src/main/java/pl/vistula/Simplecalculater.java
@@ -3,21 +3,21 @@ package pl.vistula;
 public class Simplecalculater implements Calculator {
     @Override
     public double add(double first, double second) {
-        return 0;
+        return first + second;
     }
 
     @Override
     public double sub(double first, double second) {
-        return 0;
+        return first - second;
     }
 
     @Override
     public double mul(double first, double second) {
-        return 0;
+        return first * second;
     }
 
     @Override
     public double div(double first, double second) {
-        return 0;
+        return first / second ;
     }
 }
